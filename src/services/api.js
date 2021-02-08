6d1b906f6bd5bd45215b7fadd68777e657fa3050
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-02-08 19:37:32 -0300

    Arquivo para requisições a api

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://mppro.com.br/api/pig'
+    baseURL: 'https://mppro.com.br/api'
 });
 
 export default {
-    ping: async (req, res) => {
+    ping: async () => {
         let { data: json } = await api.get('/ping');
         return json;
     }
