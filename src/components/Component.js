6282d59e5d81b8b54daada91c6af1e65f005aeb3
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-02-10 15:42:08 -0300

    Components

---
+++ src/components/Component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1;
+`;
+const Text = styled.Text``;
+
+export default () => {
+    return (
+        <Container>
+            <Text>Component</Text>
+        </Container>
+    );
+}
